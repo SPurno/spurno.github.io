0965d8991a5f6f20e4629114bcbe7c7cad42ef64
SPurno <any_dj@live.com>
2024-06-27 12:50:25 +0600

    syntx error solve

--- projects/sd/js/custom.js
+++ projects/sd/js/custom.js
@@ -8,7 +8,7 @@
 			if (window.scrollY > 1800) {
 				document.getElementById('navbar_top').classList.add('fixed-top');
 				// add padding top to show content behind navbar
-				navbar_height = document.querySelector('.navbar').offsetHeight;
+				navbar_height = document.querySelector('.navbar')?.offsetHeight;
 				document.body.style.paddingTop = navbar_height + 'px';
 			} else {
 				document.getElementById('navbar_top').classList.remove('fixed-top');
